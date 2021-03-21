cc3a29715b80fa0ce2a66f5eb1e8b5aa052bd7e1
Spedoske <leomundspedoske@gmail.com>
2021-03-21 15:39:21 +0800

    Finish anime generation.

--- gen_data.py
+++ gen_data.py
@@ -1,18 +1,30 @@
 import h5py
 import pandas as pd
 import numpy as np
+import os
+import glob
 
 
-def readH5(filename):
-    dataframes = []
+def readH5(filename, save_dir_name):
     f = h5py.File(filename, 'r')
+
     def visitandsave(name):
         ele = f[name]
         if isinstance(ele, h5py.Dataset):
-            dataframes.append(pd.DataFrame(np.array(ele)))
+            data = pd.DataFrame(np.array(ele))
+            data.to_pickle(os.path.join(save_dir_name, name.replace('/', '_')))
+            print(data.keys())
+
     f.visit(visitandsave)
-    return dataframes
 
+readH5('data/overhang/bae8f52c-407e-5f89-a8e3-61fcca51ee0a.h5','data/overhang_exported')
+readH5('data/overhang/bae8f52c-407e-5f89-a8e3-61fcca51ee0a_raw.h5','data/overhang_exported')
 
-
-
+readH5('data/traverse/e897d166-1618-5bd3-ba3a-cb7577c64647.h5','data/traverse_exported')
+readH5('data/traverse/e897d166-1618-5bd3-ba3a-cb7577c64647_raw.h5','data/traverse_exported')
+# filelist = ['data/overhang', 'data/traverse']
+# for ff in filelist:
+#     for q in glob.glob(os.path.join(ff, '*.h5')):
+#         print(q)
+#         os.mkdir(ff + "_exported")
+#         readH5(q, ff + "_exported")
--- highligh_finder.py
+++ highligh_finder.py
@@ -1,28 +1,113 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-acc_LH = pd.read_pickle('data/vertical_exported/acc_RH')
-
-acc_fps_begin = 1865 + 10 * 50
-acc_fps_end = 1865 + 83 * 50
+import matplotlib.animation as animation
 
 
-def get_time(t):
-    return (t - 1865) / 50
+def cross_entropy(a):
+    return a.cumsum()
 
 
-fps = 50
-scan_size = 2 * fps
-acc_abs_max = set()
-for i in range(acc_fps_begin, acc_fps_end - scan_size, scan_size // 2):
-    data = acc_LH[i:i + scan_size]
-    s = np.sqrt(data['AP']**2+data['DP']**2+data['UR']**2)
-    result = (s.max(), s.min(), i + np.argmax(s), i + np.argmin(s))
-    acc_abs_max.add(result)
-acc_abs_max_list = list(acc_abs_max)
-acc_abs_max_list = sorted(acc_abs_max_list, key=lambda e: -abs(e[0] - e[1]))
-for i in range(15):
-    # print(acc_abs_max_list[i])
-    print("{:.3}--{:.3}".format(get_time(acc_abs_max_list[i][2]), get_time(acc_abs_max_list[i][3])))
+def get_hl(name):
+    print(name)
+    namel = 'data/{}_exported/acc_LH'.format(name)
+    namer = 'data/{}_exported/acc_RH'.format(name)
+
+    def side(name_s):
+        acc_LH = pd.read_pickle(name_s)
+
+        acc_fps_begin = 1865 + 10 * 50
+        acc_fps_end = 1865 + 83 * 50
+
+        def get_time(t):
+            return (t - 1865) / 50
+
+        fps = 50
+        scan_size = 2 * fps
+        acc_abs_max = set()
+        for i in range(acc_fps_begin, acc_fps_end - scan_size, scan_size // 2):
+            data = acc_LH[i:i + scan_size]
+            s = np.sqrt(data['AP'] ** 2 + data['DP'] ** 2 + data['UR'] ** 2)
+            result = (s.max(), s.min(), i + np.argmax(s), i + np.argmin(s))
+            acc_abs_max.add(result)
+        acc_abs_max_list = list(acc_abs_max)
+        acc_abs_max_list = sorted(acc_abs_max_list, key=lambda e: -abs(e[0] - e[1]))
+        for i in range(6):
+            # print(acc_abs_max_list[i])
+            print("{:.3}--{:.3}".format(get_time(acc_abs_max_list[i][2]), get_time(acc_abs_max_list[i][3])))
+
+    side(namel)
+    side(namer)
+    print("------------------------------------------")
+
+
+def get_highlight_graph(left_hand, right_hand):
+    fps = 30
+    acc_fps = 50
+
+    acc_fps_begin = 1865
+
+    acc_lh = pd.read_pickle(left_hand)  # 'data/vertical_exported/acc_LH')
+    acc_rh = pd.read_pickle(right_hand)
+
+    endFrame = acc_fps_begin + 90 * fps
+
+    save_count = endFrame - acc_fps_begin
+
+    def getFrameCount():
+        for i in range(int(acc_fps_begin / acc_fps * fps), int(acc_lh.shape[0] / acc_fps * fps)):
+            yield i
+
+    def init():
+        ax.set_ylim(0, 5)
+        ax.set_xlim(0, 1)
+        del xdata[:]
+        del ydata[:]
+        line.set_data(xdata, ydata)
+        return line,
+
+    fig, ax = plt.subplots()
+    line, = ax.plot([], [], lw=2)
+    ax.grid()
+    xdata, ydata = [], []
+
+    def run(frameCount):
+        second = frameCount / fps
+        accframeCount = int(second * acc_fps)
+        # update the data
+        print(second - acc_fps_begin / acc_fps)
+
+        # here we gen the data
+
+        lap = acc_lh[max(accframeCount - acc_fps, 0):accframeCount]['AP']
+        lur = acc_lh[max(accframeCount - acc_fps, 0):accframeCount]['UR']
+        ldp = acc_lh[max(accframeCount - acc_fps, 0):accframeCount]['DP']
+
+        rap = acc_rh[max(accframeCount - acc_fps, 0):accframeCount]['AP']
+        rur = acc_rh[max(accframeCount - acc_fps, 0):accframeCount]['UR']
+        rdp = acc_rh[max(accframeCount - acc_fps, 0):accframeCount]['DP']
+
+        l = np.sqrt(lap ** 2 + lur ** 2 + ldp ** 2)
+        r = np.sqrt(rap ** 2 + rur ** 2 + rdp ** 2)
+
+        ydata = np.array(l + r)
+        # gen data end
+
+        xdata = np.linspace(0, 1, acc_fps)[0:len(ydata)]
+        line.set_data(xdata, ydata)
+
+        return line,
+
+    ani = animation.FuncAnimation(fig, run, getFrameCount, interval=1000 / fps, init_func=init, save_count=save_count)
+    #plt.show()
+    ani.save(left_hand.replace('/', '_') + '_' + 'highlight' + '.mp4', writer='ffmpeg', fps=fps, dpi=300)
+
+def get_height_graph(height_path):
+    height = pd.read_pickle(height_path)
+    height.plot(label='height')
+
+# for f in [('data/overhang_exported/acc_LH','data/overhang_exported/acc_RH'), ('data/traverse_exported/acc_LH','data/traverse_exported/acc_RH'), ('data/traverse_exported/acc_LH','data/traverse_exported/acc_RH')]:
+# for f in [('data/vertical_exported/acc_LH','data/vertical_exported/acc_RH')]:
+#     get_highlight_graph(f[0],f[1])
+get_height_graph('data/vertical_exported/climbs_0_height_profile')
 
